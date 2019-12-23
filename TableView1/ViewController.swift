ed8b911aac65976356b583f0f7617b1dc0bc63d3
shanth kumar <shanth923@gmail.com>
2019-12-23 16:24:59 +0530

    Initial Commit

---
+++ TableView1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableView1
+//
+//  Created by R Shantha Kumar on 12/23/19.
+//  Copyright © 2019 R Shantha Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
